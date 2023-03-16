4e5dbf1f491ed345ff9d35eea25541afa07ba083
Zukisani <ZukisaniMemela@transnet.net>
2023-03-16 12:45:45 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+        printf("Size of a char: %lu byte(s)"), sizef(char));
+	printf("size of int: %lu bytes(s)", sizeof(int));
+        return (0);
+}
+
